102e2ad448d02c07790b8f06ddf2a697008cd7c3
Lokijopu <kguo2134@gmail.com>
2019-01-03 13:37:17 -0500

    Add files via upload

--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * 
  * @author Kevin Guo Period 6
@@ -6,7 +8,41 @@
 public class BankAccountMain {
 
 	public static void main(String[] args) {
-
+		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+		accounts.add(new CheckingAccount("Mr. Ellis", 100, 30, 1, 10));
+		accounts.add(new CheckingAccount("Mrs. Ellis",30,1,10));
+		accounts.add(new SavingsAccount("Mr. Ellis", 500, .025, 200,5));
+		accounts.add(new SavingsAccount("Mrs. Ellis", .025, 200,5));
+		Scanner in = new Scanner(System.in);
+		System.out.print("Would you like to \nadd an account \nmake a transaction \nterminate the program\n\t" );
+		String str1 = in.nextLine().toLowerCase();
+		while (!str1.equals("add an account") && !str1.equals(("make a transaction")) && !str1.equals(("terminate the program"))) {
+			System.out.print("Would you like to \nadd an account \nmake a transaction \nterminate the program\nInput your choice exactly:\t" );
+			str1 = in.nextLine();
+		}
+		if (str1.equals("add an account")) {
+			System.out.print("checking or savings?");
+			String str2 = in.nextLine().toLowerCase();
+			while (!str2.equals("checking") && !str2.equals("saving")) {
+				System.out.print("checking or savings?");
+				str2 = in.nextLine().toLowerCase();
+			}
+			if (str2.equals("checking")) {
+				System.out.print("name\t");
+				String name = in.nextLine();
+				System.out.print("balance\t");
+				double bal = in.nextDouble();
+			} else if (str2.equals("savings")) {
+				System.out.print("name\t");
+				String name = in.nextLine();
+				System.out.print("balance\t");
+				double bal = in.nextDouble();
+			}
+		} else if (str1.equals(("make a transaction"))) {
+			System.out.println("2");
+		} else if (str1.equals(("terminate the program"))) {
+			System.out.println("Have a nice day.");
+		} 
 	}
 
 }
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,67 @@
+/**
+ * 
+ * @author Kevin Guo Period 6
+ *
+ */
+public class SavingsAccount extends BankAccount{
 
-public class SavingsAccount {
-
+	private double intRate;
+	private static double MIN_BAL;
+	private static double MIN_BAL_FEE;
+	SavingsAccount(String n, double b, double r, double mb, double mbf) {
+		super(n, b);
+		intRate = r;
+		MIN_BAL = mb;
+		MIN_BAL_FEE = mbf;
+	}
+	
+	SavingsAccount(String n, double r, double mb, double mbf) {
+		super(n, 0);
+		intRate = r;
+		MIN_BAL = mb;
+		MIN_BAL_FEE = mbf;
+	}
+	
+	public void withdraw(double amt) {
+		try {
+			if (this.getBalance() - amt >= MIN_BAL) {
+				this.withdraw(amt);
+			} else if (this.getBalance() - amt < MIN_BAL && this.getBalance() - amt >= MIN_BAL_FEE) {
+				this.withdraw(amt);
+				this.withdraw(MIN_BAL_FEE);
+			}
+		}
+		catch (IllegalArgumentException e) {
+			if (this.getBalance() <= amt + MIN_BAL_FEE) {
+			throw new IllegalArgumentException("Transaction not authorized; IllegalArgumentException");
+			}
+		}
+		catch (NullPointerException e) {
+			throw new NullPointerException("NullPointerException");
+		}
+	}
+	
+	public void transfer(BankAccount other, double amt) {
+		try {
+			if (this.getName().equals(other.getName()) && this.getBalance() >= amt) {
+				this.transfer(other, amt);
+			}
+		}
+		catch (IllegalArgumentException e) {
+			if (!this.getName().equals(other.getName()) || (this.getBalance() < amt)) {
+				throw new IllegalArgumentException("Transaction is not allowed to occur IllegalArgumentException");
+			}
+		}
+		catch (NullPointerException e) {
+			throw new NullPointerException("NullPointerException");
+		}
+	}
+	
+	public void addInterest() {
+		this.deposit(this.getBalance() * intRate); 
+	}
+	public void endOfMonthUpdate() {
+		this.addInterest();
+	}
+	
 }
